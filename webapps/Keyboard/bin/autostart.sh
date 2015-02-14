49e91c8fcb1d91cdfd2f74f850ebd84e155763b2
Bill Welliver <hww3@riverweb.com>
2015-02-14 10:37:08 -0500

    update startup script for new host.

--- webapps/Keyboard/bin/autostart.sh
+++ webapps/Keyboard/bin/autostart.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 PATH=/usr/local/pike/7.9.5/bin:$PATH
 export PATH
-FINS_HOME=/srv/delta-home/keyboard-deploy/Keyboard
+FINS_HOME=$HOME/kb/keyboard-deploy/Keyboard
 export FINS_HOME
 while /bin/true ; 
 do
-  $FINS_HOME/bin/start.sh -p8080 --no-virtual
+  $FINS_HOME/bin/start.sh -p8089 --no-virtual
   sleep 10
 done;
